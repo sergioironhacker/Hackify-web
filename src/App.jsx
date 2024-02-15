59d11f5fb0d242c981e23f4020fd59c438c87e89
Sergio Esteban <Sergioss772022@gmail.com>
2024-02-15 20:35:44 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 
 function App() {
 
-
   return (
-    <>
-   
-    </>
+    <div>
+  
+
+      <div className="">
+        <Routes>
+          <Route path="/" element={<Home />} />
+      
+      
+        </Routes>
+      </div>
+    </div>
   )
 }
 
