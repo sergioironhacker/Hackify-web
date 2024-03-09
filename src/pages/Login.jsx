917151a81df544d4c954d760741510436b34cca9
sergioironhacker <sergioss772022@gmail.com>
2024-03-09 09:41:36 +0100

    login and register style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,9 @@
-import Input from "../components/Input";
 import { useFormik } from 'formik';
-import { string, object } from 'yup';
-import Button from "../components/Button";
-import { useContext } from "react";
-import AuthContext from "../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
+import Input from "../components/Input";
+import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
+import { object, string } from 'yup';
+import AboutUsLogo from '../assets/AboutUsLogo'; // Importa el componente del logo
 
 const userSchema = object({
   email: string().email('Enter a valid email').required('Required field'),
@@ -12,61 +11,61 @@ const userSchema = object({
 });
 
 const Login = () => {
-  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
-
-  const { values, errors, touched, isValid, handleBlur, handleChange, handleSubmit } = useFormik({
+  const { values, errors, touched, isValid, handleSubmit, handleChange, handleBlur } = useFormik({
     initialValues: {
       email: '',
       password: ''
     },
-    onSubmit: (values) => {
-      login(values)
-        .then(() => navigate('/profile'))
+    onSubmit: async (values) => {
+      try {
+        // Lógica de inicio de sesión
+        navigate('/profile');
+      } catch (error) {
+        console.error('Error logging in:', error.message);
+      }
     },
     validationSchema: userSchema,
-    validateOnChange: true,
     validateOnBlur: true,
     validateOnMount: true,
   });
 
   return (
-    <div className="flex justify-center items-center h-screen ">
-      <div className="w-full max-w-md p-6 bg-white shadow-md rounded-md ">
-        <h1 className="text-green-400 uppercase font-bold text-3xl mb-6 ">Sign in to your account</h1>
+    <div className="flex justify-center items-center h-screen text-green-400">
+      <div className="w-full max-w-md p-6 bg-white shadow-md rounded-md">
+        <h1 className="flex justify-center text-2xl font-bold text-green-400 mb-4 ">
+          Hackify <AboutUsLogo />
+        </h1>
+        <h3 className="text-green-400 uppercase font-bold text-3xl mb-6 italic">Login</h3>
         
         <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 gap-4 text-green-400 ">
+          <div className="grid grid-cols-1 gap-4">
             <Input
               name="email"
               type="email"
               label="Email"
-              placeholder="Enter your email"
+              placeholder="Ex: 'example@example.com'"
               value={values.email}
               error={touched.email && errors.email}
               onChange={handleChange}
               onBlur={handleBlur}
-              textClassName="text-green-400"
-              extraClassName="border-green-400 focus:border-green-400"
             />
             <Input
               name="password"
               type="password"
               label="Password"
-              placeholder="Enter your password"
+              placeholder="Ex: '********'"
               value={values.password}
               error={touched.password && errors.password}
               onChange={handleChange}
               onBlur={handleBlur}
-              textClassName="text-green-400"
-              extraClassName="border-green-400 focus:border-green-400"
             />
           </div>
-          <Button extraClassName="mt-6 bg-green-400" disabled={!isValid} text="Sign in" />
+          <Button extraClassName="mt-6 w-full text-green-400" text="Sign in" disabled={!isValid} />
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,6 +4,7 @@ import Button from '../components/Button';
 import { useNavigate } from 'react-router-dom';
 import { register } from '../services/AuthService';
 import { object, string, mixed } from 'yup';
+import AboutUsLogo from '../assets/AboutUsLogo'; // Importa el componente del logo
 
 const userSchema = object({
   username: string().required('Required field'),
@@ -39,7 +40,10 @@ const Register = () => {
   return (
     <div className="flex justify-center items-center h-screen text-green-400">
       <div className="w-full max-w-md p-6 bg-white shadow-md rounded-md">
-        <h1 className="text-green-400 uppercase font-bold text-3xl mb-6">Register your account</h1>
+        <h1 className="flex justify-center text-2xl font-bold text-green-400 mb-4 ">
+          Hackify <AboutUsLogo />
+        </h1>
+        <h3 className="text-green-400 uppercase font-bold text-3xl mb-6 italic">Register</h3>
         
         <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-1 gap-4">
