ea7086f0fe8b2ba00a450d72e2643a12086b7ebd
sergioironhacker <sergioss772022@gmail.com>
2024-03-11 20:58:55 +0100

    errors in login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import Input from "../components/Input";
 import { useFormik } from 'formik';
 import { string, object } from 'yup';
 import Button from "../components/Button";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import AboutUsLogo from "../assets/AboutUsLogo";
@@ -15,15 +15,20 @@ const userSchema = object({
 const Login = () => {
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [error, setError] = useState('');
 
   const { values, errors, touched, isValid, handleBlur, handleChange, handleSubmit } = useFormik({
     initialValues: {
       email: '',
       password: ''
     },
-    onSubmit: (values) => {
-      login(values)
-        .then(() => navigate('/profile'))
+    onSubmit: async (values) => {
+      try {
+        await login(values);
+        navigate('/profile');
+      } catch (error) {
+        setError('Email or password incorrect');
+      }
     },
     validationSchema: userSchema,
     validateOnChange: true,
@@ -32,16 +37,16 @@ const Login = () => {
   });
 
   return (
-    <div className="flex justify-center items-center h-screen ">
-      <div className="w-full max-w-md p-6 bg-white shadow-md rounded-md ">
-      <div className="flex justify-center mb-4">
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full max-w-md p-6 bg-white shadow-md rounded-md">
+        <div className="flex justify-center mb-4">
           <h1 className="text-2xl font-bold text-green-400 mr-2">Hackify</h1>
           <AboutUsLogo />
         </div>
-        <h1 className="text-green-400 uppercase font-bold text-3xl mb-6 ">Login</h1>
+        <h1 className="text-green-400 uppercase font-bold text-3xl mb-6">Login</h1>
         
         <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 gap-4 text-green-400 ">
+          <div className="grid grid-cols-1 gap-4 text-green-400">
             <Input
               name="email"
               type="email"
@@ -67,6 +72,7 @@ const Login = () => {
               extraClassName="border-green-400 focus:border-green-400"
             />
           </div>
+          {error && <p className="text-red-500">{error}</p>}
           <Button extraClassName="mt-6 bg-green-400" disabled={!isValid} text="Sign in" />
         </form>
       </div>
